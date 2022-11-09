bd979f49fb6138d60a43f9cbddf2a0c9842c3a06
Mr-Maximka <113691097+Mr-Maximka@users.noreply.github.com>
2022-11-09 06:10:23 +0300

    Add files via upload

---
+++ seti2/servera/server.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <winsock2.h>
+#include <string>
+#include <windows.h>
+#include <WS2tcpip.h>
+#pragma comment (lib, "Ws2_32.lib")
+//#pragma warning(disable: 4996)
+using namespace std;
+//#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define SRV_PORT 1234 //server port
+#define BUF_SIZE 64 //buffer size
+
+struct OrderAndCost
+{
+    int productid;
+    int amount;
+};
+
+int main() {
+    
+    char buff[1024];
+
+    WSADATA wsData;
+    int erStat = WSAStartup(MAKEWORD(2, 2), &wsData);
+    if (erStat != 0) {
+        cout << "Error WinSock version initializaion # ";
+        cout << WSAGetLastError();
+        return 1;
+    }
+    else
+        cout << "WinSock initialization is OK" << endl;
+
+    SOCKET ServSock = socket(AF_INET, SOCK_STREAM, 0);
+    if (ServSock == INVALID_SOCKET) {
+        cout << "Error initialization socket # " << WSAGetLastError() << endl;
+        closesocket(ServSock);
+        WSACleanup();
+        return 1;
+    }
+    else
+        cout << "Server socket initialization is OK" << endl;
+
+    sockaddr_in servInfo;
+    ZeroMemory(&servInfo, sizeof(servInfo));
+
+    servInfo.sin_family = AF_INET;
+    servInfo.sin_addr.s_addr = 0;
+    servInfo.sin_port = htons(SRV_PORT);
+
+    erStat = bind(ServSock, (sockaddr*)&servInfo, sizeof(servInfo));
+    if (erStat != 0) {
+        cout << "Error Socket binding to server. Error # " << WSAGetLastError() << endl;
+        closesocket(ServSock);
+        WSACleanup();
+        return 1;
+    }
+    else
+        cout << "Binding socket to Server info is OK" << endl;
+
+    erStat = listen(ServSock, 10);
+    if (erStat != 0) {
+        cout << "Can't start to listen to. Error # " << WSAGetLastError() << endl;
+        closesocket(ServSock);
+        WSACleanup();
+        return 1;
+    }
+    else {
+        cout << "Listening..." << endl;
+    }
+
+    OrderAndCost* msg = new OrderAndCost;
+    //msg->amount = 0; msg->productid = 0;
+    float result;
+
+    sockaddr_in clientInfo;
+    ZeroMemory(&clientInfo, sizeof(clientInfo));
+    int clientInfo_size = sizeof(clientInfo);
+
+    short packet_size = 0;
+
+    char buf[BUF_SIZE] = { 0 };
+    while (true) {
+        SOCKET ClientConn = accept(ServSock, (sockaddr*)&clientInfo, &clientInfo_size);
+        if (ClientConn == INVALID_SOCKET) {
+            cout << "Client detected, but can't connect to a client. Error # " << WSAGetLastError() << endl;
+            closesocket(ServSock);
+            closesocket(ClientConn);
+            WSACleanup();
+            return 1;
+        }
+        else
+            cout << "Connection to a client established successfully" << endl;
+
+        while (true) {
+            packet_size = recv(ClientConn, (char*)&buf, BUF_SIZE, 0);
+            buf[packet_size] = 0;
+            memcpy(msg, buf, sizeof(msg));
+            cout << "Message Recieved, Calculating The Result from: " << msg->productid << " " << msg->amount << endl;
+
+            if (msg->amount >= 0 && msg->productid >= 0)
+            {
+                result = msg->amount * msg->productid;
+                cout << result << endl;
+            }
+            else break;
+
+            packet_size = send(ClientConn, (char*)&result, sizeof(result), 0);
+            if (packet_size == SOCKET_ERROR) {
+                cout << "Can't send message. Error # " << WSAGetLastError() << endl;
+                closesocket(ClientConn);
+                WSACleanup();
+                return 1;
+            }
+        }
+
+        result = -1;
+        packet_size = send(ClientConn, (char*)&result, sizeof(result), 0);
+        if (packet_size == SOCKET_ERROR) {
+            cout << "Can't send message. Error # " << WSAGetLastError() << endl;
+            closesocket(ClientConn);
+            WSACleanup();
+            return 1;
+        }
+
+        cout << "The Client has disconnected" << endl;
+        closesocket(ClientConn);
+    }
+    return 0;
+}
